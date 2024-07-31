2165ab4e62a4f4d1624ce7d7c8bd5ce52e79efd5
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-31 16:23:32 +0530

    Create Layout.tsx

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import ProfileModal from '../components/profile'
+const Layout = () => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+    return (
+        <>
+            <header className='h-20 w-full bg-gray-950 flex items-center gap-3 fixed'>
+                <ul className='w-full flex  justify-end gap-x-4 px-4 '>
+                    <li className='p-2 w-[60px] min-w-[60px] bg-coloumnBackgroundColor rounded-lg text-center'>
+                        Join
+                    </li>
+                    <li className='p-2 bg-coloumnBackgroundColor w-[100px] min-w-[100px] rounded-lg text-center'>
+                        Broadcast
+                    </li>
+                    <li
+                       onClick={toggleModal}
+                        className='p-2 bg-coloumnBackgroundColor rounded-lg w-[60px] min-w-[60px] text-center'>
+                        Profile
+                    </li>
+                    {showModal && <ProfileModal onClose={toggleModal} isOpen={showModal} />}
+
+                </ul>
+
+            </header>
+            <main className=''>
+                <Outlet />
+            </main>
+            <footer></footer>
+        </>
+
+    )
+}
+
+export default Layout
