59842df601d20f4fe327bf19ce49e29adb16c7e7
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-08-01 16:09:41 +0530

    Create AuthLayout.tsx

---
+++ src/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+interface ProtectedRouteProps {
+    children: React.ReactNode; // Define children as React.ReactNode
+  }
+const ProtectedRoute: React.FC<ProtectedRouteProps>  = ({ children }) => {
+  const location = useLocation();
+  const isAuthenticated = !!localStorage.getItem('jwt'); 
+
+  if (!isAuthenticated) {
+   
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
